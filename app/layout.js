be20f11875f73e7e967f29e8e3a4134573b0fc52
mmgonnar <gn.mariela@gmail.com>
2025-04-25 17:02:04 -0600

    Refactored menu items & created constants.js

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,7 @@
 import { Inter } from 'next/font/google';
-
-import Header from '@/components/Header/Header';
 import './globals.css';
-import Footer from '@/components/Header/Footer';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
 export const metadata = {
   title: 'ClassAid App',
--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <div className="font-main h-60 bg-blue-100 pt-16">Hello world!</div>
+      <div className="font-main h-60 bg-blue-100 pt-30">Hello world!</div>
     </main>
   );
 }
