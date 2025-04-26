e39c2e3b62164888ce3fe0001934db84570c4632
mmgonnar <gn.mariela@gmail.com>
2025-04-26 10:58:21 -0600

    Updated mainButton style

--- components/MainButton.js
+++ components/MainButton.js
@@ -24,25 +24,25 @@ function MainButton(props) {
   // };
 
   return (
-    <>
+    <section className="m-20">
       <Link href="/" className="w-full">
-        <button className="text-primary hover:bg-primary border-primary w-[150px] cursor-pointer rounded-full border bg-transparent px-4 py-1 font-bold hover:border-transparent hover:text-white">
+        <button className="text-primary hover:bg-primary border-primary w-[150px] cursor-pointer rounded-full border bg-transparent px-4 py-1 font-bold shadow-lg hover:border-transparent hover:text-white">
           {/* {variantsText[text]} */} Try for free!
         </button>
       </Link>
 
       <Link href="/" className="w-full">
-        <button className="bg-primary w-[150px] cursor-pointer rounded-full py-1 font-bold text-white hover:bg-blue-800">
+        <button className="bg-primary w-[150px] cursor-pointer rounded-full py-1 font-bold text-white shadow-lg hover:bg-blue-700">
           {/* {variantsText[text]} */}More info
         </button>
       </Link>
 
       <Link href="/" className="w-full">
-        <button className="bg-primary hover:border-primary hover:text-primary w-[150px] cursor-pointer rounded-full border px-4 py-1 font-bold text-white hover:bg-transparent">
+        <button className="bg-primary hover:border-primary hover:text-primary w-[150px] cursor-pointer rounded-full px-4 py-1 font-bold text-white shadow-lg hover:border hover:bg-transparent">
           {/* {variantsText[text]} */} Try for free!
         </button>
       </Link>
-    </>
+    </section>
   );
 }
 
