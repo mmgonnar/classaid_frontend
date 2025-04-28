8fa52a011a3bc2bcc386c6393640b6e8e3590e14
mmgonnar <gn.mariela@gmail.com>
2025-04-28 15:23:52 -0600

    Fixed MainButton component with additional variants

--- components/MainButton.js
+++ components/MainButton.js
@@ -1,47 +1,26 @@
-import Link from 'next/link';
-import React from 'react';
+'use client';
 
 function MainButton(props) {
-  // const { variant = 'primary', text = 'primary' } = props;
-  // if (variant == 'pimary') {
-  //   ('clases');
-  // } else {
-  //   ('otras clases');
-  // }
+  const { variant = 'primary', text = '', link } = props;
 
-  // {
-  //   ['muchos condiconales'];
-  // }
+  const variants = {
+    primary:
+      'bg-primary w-[150px] cursor-pointer rounded-full py-1 font-bold text-white shadow-lg hover:bg-blue-800',
+    secondary:
+      'text-primary hover:bg-primary border-primary w-[150px] cursor-pointer rounded-full border bg-transparent px-4 py-1 font-bold shadow-lg hover:border-transparent hover:text-white',
+    destructive:
+      'bg-primary hover:border-primary hover:text-primary w-[150px] cursor-pointer rounded-full px-4 py-1 font-bold text-white shadow-lg hover:border hover:bg-transparent',
+  };
 
-  // const variants = {
-  //   primary: '',
-  //   secondary: '',
-  // };
-  // const variantsText = {
-  //   primary: '',
-  //   secondary: '',
-  //   destructive: '',
-  // };
+  return <button className={`${variants[variant]}`}>{text}</button>;
+}
 
+export function ButtonSection() {
   return (
-    <section className="m-20">
-      <Link href="/" className="w-full">
-        <button className="text-primary hover:bg-primary border-primary w-[150px] cursor-pointer rounded-full border bg-transparent px-4 py-1 font-bold shadow-lg hover:border-transparent hover:text-white">
-          {/* {variantsText[text]} */} Try for free!
-        </button>
-      </Link>
-
-      <Link href="/" className="w-full">
-        <button className="bg-primary w-[150px] cursor-pointer rounded-full py-1 font-bold text-white shadow-lg hover:bg-blue-700">
-          {/* {variantsText[text]} */}More info
-        </button>
-      </Link>
-
-      <Link href="/" className="w-full">
-        <button className="bg-primary hover:border-primary hover:text-primary w-[150px] cursor-pointer rounded-full px-4 py-1 font-bold text-white shadow-lg hover:border hover:bg-transparent">
-          {/* {variantsText[text]} */} Try for free!
-        </button>
-      </Link>
+    <section className="flex-col gap-1">
+      <MainButton variant="primary" text="Example" />
+      <MainButton variant="secondary" text="Example" />
+      <MainButton variant="destructive" text="Example" />
     </section>
   );
 }
