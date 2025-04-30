9fb895250a04332ff6dfbfdd42a4e6b64afc1819
mmgonnar <gn.mariela@gmail.com>
2025-04-29 19:52:06 -0600

    feat: :construction: Added TextTemplate component & added third color in globals.css

--- app/page.js
+++ app/page.js
@@ -2,13 +2,17 @@ import MainButton, { ButtonSection } from '@/components/MainButton';
 import CTASection from '@/components/sections/CTASection';
 import Banner from '@/components/Banner';
 import { CTA } from '@/utils/enums';
+import TextTemplate from '@/components/sections/TextTemplate';
 
 export default function Home() {
   return (
     <main className="">
       <Banner />
+
       <ButtonSection />
       <CTASection />
+      <TextTemplate />
+      <TextTemplate align="left" />
       <CTASection title="afdsadasd" subtitle="asdasdasds" buttonText={CTA.LEARN_MORE} />
     </main>
   );
