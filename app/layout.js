0f1af9465a6f600118b8ca2d1948fc8e1c3c3987
mmgonnar <gn.mariela@gmail.com>
2025-05-06 14:24:33 -0600

    feat: :art: Styled SignIn page and removed unused component

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,5 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
 
 export const metadata = {
   title: 'ClassAid App',
--- app/signin/page.js
+++ app/signin/page.js
@@ -3,23 +3,28 @@ import { CTA } from '@/utils/enums';
 import Link from 'next/link';
 
 import LoginForm from '@/components/forms/LoginForm';
-import RegisterForm from '@/components/forms/RegisterForm';
 
 function Login() {
   return (
-    <section className="m-auto flex bg-[linear-gradient(345deg,rgba(101,156,246,0.3),rgba(246,246,246,0.2))]">
-      <div className="hidden text-neutral-700 md:block">
-        <h1>dasdasdasdas</h1>
-        <p>asdasdasdasdas</p>
+    <section className="flex min-h-screen items-center justify-center bg-[linear-gradient(345deg,rgba(101,156,246,0.3),rgba(246,246,246,0.2))]">
+      <div className="container mx-auto hidden max-w-4xl flex-col justify-between gap-6 px-30 text-neutral-700 md:flex md:w-3/5">
+        <h1 className="text-3xl font-semibold">A cool base line goes here!</h1>
+        <h2 className="text-xl font-semibold">Another catch phrase to punch the message</h2>
+        <p className="font-base w-auto pb-10 text-lg">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget
+          dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,
+          nascetur ridiculus mus
+        </p>
+        <MainButton variant="secondary" text={CTA.LEARN_MORE} />
       </div>
-      <div className="flex flex-col gap-4 rounded rounded-s-lg bg-white p-6 md:w-[50%] md:place-self-end">
+      <div className="flex flex-col justify-center gap-4 rounded rounded-s-xl bg-white p-6 shadow-2xl md:min-h-screen md:w-2/5 md:p-20">
         <Link href="/">
           <h2 className="text-primary text-base font-bold">
             Classs<span className="font-black">Aid</span>
           </h2>
         </Link>
-        <div className="text-neutral-700">
-          <h3 className="text-xl">Welcome back!</h3>
+        <div className="pb-7 text-neutral-700">
+          <h3 className="pb-2 text-xl md:pb-4">Welcome back!</h3>
           <p className="text-base">
             Please use your credentials to login. <br />
             If you are not a member, please{' '}
