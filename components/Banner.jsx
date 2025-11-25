c1e3f05d241df5736b57830140259914e4f37b82
mmgonnar <gn.mariela@gmail.com>
2025-11-25 14:10:45 -0600

    feat: Frontend changes

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -7,9 +7,11 @@ import { TypeAnimation } from 'react-type-animation';
 
 function Title() {
   return (
-    <section className="relative top-[-50px] bg-[linear-gradient(345deg,rgba(101,156,246,0.3),rgba(246,246,246,0.2))] pt-20 pb-60 md:pt-40 md:pb-50">
-      {/* <div className="bg-[url(/img/classroom.jpg)]"></div> */}
-      <div className="mx-auto pt-18 sm:px-6 sm:py-10">
+    <section className="relative top-[-50px] pt-20 pb-60 md:pt-40 md:pb-50">
+      {/* Background image with gradient overlay */}
+      <div className="absolute inset-0 bg-[url(/classroom.jpg)] bg-cover bg-center bg-no-repeat opacity-30"></div>
+      <div className="absolute inset-0 bg-[linear-gradient(345deg,rgba(101,156,246,0.3),rgba(246,246,246,0.2))]"></div>
+      <div className="relative mx-auto pt-18 sm:px-6 sm:py-10">
         <div className="relative text-center">
           <h1 className="text-xl font-bold text-gray-900 sm:text-5xl">
             Simplify.
@@ -26,7 +28,7 @@ function Title() {
             Empower.
           </h1>
 
-          <p className="mt-4 text-base text-pretty text-gray-700 md:text-lg/relaxed">
+          <p className="mt-4 text-base text-pretty text-gray-900 md:text-lg/relaxed">
             Your digital ally to simplify your class management and transform teaching.
           </p>
 
